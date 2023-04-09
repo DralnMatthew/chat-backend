d0e04a1284139604a5841619c4ec1e2ebe7eb70b
matt <zhanghu.zhao@outlook.com>
2023-04-09 16:48:24 +0300

    feature: implemented auth feature

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -7,13 +7,13 @@ import compression from 'compression';
 import cookieSession from 'cookie-session';
 import HTTP_STATUS from 'http-status-codes';
 import 'express-async-errors';
-import { config } from './config';
 import { Server } from 'socket.io';
 import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
-import applicationRoutes from './routes';
-import { CustomError, IErrorResponse } from './shared/globals/helpers/error-handler';
+import applicationRoutes from '@root/routes';
 import Logger from 'bunyan';
+import { CustomError, IErrorResponse } from '@global/helpers/error-handler';
+import { config } from '@root/config';
 
 const SERVER_PORT = 5000;
 const log: Logger = config.createLogger('server');
